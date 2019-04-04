61dce2c0deac98a10fe8fb02601eda95c26e3a10
Yu Zhao <yu.zhao@joudou.com>
2019-04-04 11:15:39 +0800

    init commit

---
+++ chap6/the-statement-script.sh
@@ -0,0 +1,11 @@
+sed '
+#capitalize statement names
+/the .* statement/ {
+h
+s/.*the \(.*\) statement.*/\1/
+y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/
+G
+s/\(.*\)\n\(.*the \).*\( statement.*\)/\2\1\3/
+s/^/starting /
+s/$/.../
+}' the-statement.txt
